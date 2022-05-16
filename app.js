53d85c28b871906f22c59817be42a524d2eede21
matanLevi404 <matan.levi.404@gmail.com>
2022-05-16 20:27:00 +0300

    Edited app js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("./dist/hagay-portfolio"));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "/dist/hagay-portfolio/index.html"));
 });
 
